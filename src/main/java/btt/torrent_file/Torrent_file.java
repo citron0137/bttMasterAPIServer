b10856b35abf41e701db2c835db5e9e60d347154
Ubuntu <ubuntu@ip-172-31-23-241.ap-northeast-2.compute.internal>
2018-11-09 13:06:15 +0000

    small modify

--- src/main/java/btt/torrent_file/Torrent_file.java
+++ src/main/java/btt/torrent_file/Torrent_file.java
@@ -19,4 +19,24 @@ public class Torrent_file {
 
     public Torrent_file(){}
 
+    public Torrent_file(int post_seq, String magnet, String infohash, String file_size, int state){
+	this.post_seq = post_seq;
+	this.magnet = magnet;
+	this.infohash = infohash;
+	this.file_size = file_size;
+	this.state = state;
+    }
+
+    public Torrent_file(int seq, int post_seq, String magnet, String infohash, String file_size, int state){
+	this.seq = seq;
+	this.post_seq = post_seq;
+	this.magnet = magnet;
+	this.infohash = infohash;
+	this.file_size = file_size;
+	this.state = state;
+    }
+
+    
+
+
 }
